b222ae9d7c8f164d92fe924d3f9887e78e05232b
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2022-12-03 08:24:53 +0100

    Add day 3 solution

--- src/day03.rs
+++ src/day03.rs
@@ -1,25 +1,72 @@
-pub fn part1(input: &str) -> i64 {
-    input.len() as i64
+trait Priority {
+    fn priority(&self) -> u64;
 }
 
-pub fn part2(input: &str) -> i64 {
-    input.len() as i64
+impl Priority for char {
+    fn priority(&self) -> u64 {
+        if self.is_lowercase() {
+            *self as u64 - 'a' as u64 + 1
+        } else {
+            *self as u64 - 'A' as u64 + 27
+        }
+    }
+}
+
+pub fn part1(input: &str) -> u64 {
+    input
+        .split('\n')
+        .map(|sack| {
+            let front = &sack[..sack.len() / 2];
+            let rear = &sack[sack.len() / 2..];
+
+            for c in front.chars() {
+                if rear.contains(c) {
+                    return c.priority();
+                }
+            }
+
+            0
+        })
+        .sum()
+}
+
+pub fn part2(input: &str) -> u64 {
+    let elves: Vec<&str> = input.split('\n').collect();
+    let groups = elves.chunks(3);
+
+    let mut sum: u64 = 0;
+
+    for group in groups {
+        for c in group[0].chars() {
+            if group[1].contains(c) && group[2].contains(c) {
+                sum += c.priority();
+                break;
+            }
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
 mod tests {
     use crate::day03::{part1, part2};
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+                              jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+                              PmmdzqPrVvPwwTWBwg\n\
+                              wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+                              ttgJtRGJQctTZtZT\n\
+                              CrZsJsPPZsGzwwsLwLmpwMDw\n";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 0);
+        assert_eq!(part1(TEST_INPUT), 157);
     }
     
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 0);
+        assert_eq!(part2(TEST_INPUT), 70);
     }
 }
 
