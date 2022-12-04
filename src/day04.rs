3c5394cb56bcb307207665898860e2824f7cea97
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2022-12-04 07:56:38 +0100

    Add day 4 solution

--- src/day04.rs
+++ src/day04.rs
@@ -1,27 +1,58 @@
 // solution template
 
 pub fn part1(input: &str) -> u64 {
-    input.len() as u64
+    input
+        .trim()
+        .split(['\n', '-', ','])
+        .map(|num| num.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>()
+        .chunks(4)
+        .filter(|row| {
+            let (l, r) = ((row[0], row[1]), (row[2], row[3]));
+
+            (l.0 >= r.0 && l.1 <= r.1) || (r.0 >= l.0 && r.1 <= l.1)
+        })
+        .count() as u64
 }
 
 pub fn part2(input: &str) -> u64 {
-    input.len() as u64
+    input
+        .trim()
+        .split(['\n', '-', ','])
+        .map(|num| num.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>()
+        .chunks(4)
+        .filter(|row| {
+            let (l, r) = ((row[0], row[1]), (row[2], row[3]));
+
+            (l.0 >= r.0 && l.0 <= r.1)
+            || (r.0 >= l.0 && r.0 <= l.1)
+            || (l.1 <= r.1 && l.1 >= r.0)
+            || (r.1 <= l.1 && r.1 >= l.0)
+        })
+        .count() as u64
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day00::{part1, part2};
+    use crate::day04::{part1, part2};
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 0);
+        assert_eq!(part1(TEST_INPUT), 2);
     }
     
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 0);
+        assert_eq!(part2(TEST_INPUT), 4);
     }
 }
 
