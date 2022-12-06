321655d7c4bebfaaa8024a3e53d84a6d57541048
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2022-12-06 07:16:51 +0100

    Prepare for day 7

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,11 @@
 use std::env;
 
-use solutions::day06::{part1, part2};
+use solutions::day07::{part1, part2};
 
 fn main() {
     let session_key = env::var("AOC_SESSION").unwrap();
     let client = rudolf_rs::Client::new(session_key);
-    let input = client.get(2022, 6).unwrap();
+    let input = client.get(2022, 7).unwrap();
 
     println!("{}", part1(&input));
     println!("{}", part2(&input));
--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,5 @@ pub mod day03;
 pub mod day04;
 pub mod day05;
 pub mod day06;
+pub mod day07;
 
