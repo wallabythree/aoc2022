f09d2d368b8ba8968b1bf5ba2b51bf3faa01643d
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2022-12-11 10:17:26 +0100

    Add day 11 from main

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,11 @@
 use std::env;
 
-use solutions::day10::{part1, part2};
+use solutions::day11::{part1, part2};
 
 fn main() {
     let session_key = env::var("AOC_SESSION").unwrap();
     let client = rudolf_rs::Client::new(session_key);
-    let input = client.get(2022, 10).unwrap();
+    let input = client.get(2022, 11).unwrap();
 
     println!("{}", part1(&input));
     println!("{}", part2(&input));
