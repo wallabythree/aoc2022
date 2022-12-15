c5817a93150019559cd78108eac6ad1b6722e7ad
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2022-12-16 00:33:56 +0100

    Correct date for day 16

--- src/bin.rs
+++ src/bin.rs
@@ -5,7 +5,7 @@ use solutions::day16::{part1, part2};
 fn main() {
     let session_key = env::var("AOC_SESSION").unwrap();
     let client = rudolf_rs::Client::new(session_key);
-    let input = client.get(2022, 15).unwrap();
+    let input = client.get(2022, 16).unwrap();
 
     println!("{}", part1(&input));
     println!("{}", part2(&input));
