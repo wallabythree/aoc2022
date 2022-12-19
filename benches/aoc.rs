98ea5c181174599ce64a7c31b3f8b69552dc8dc0
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2022-12-19 17:22:57 +0100

    Add day 19 speeds

--- benches/aoc.rs
+++ benches/aoc.rs
@@ -347,6 +347,10 @@ fn criterion_benchmark(c: &mut Criterion) {
     );
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!{
+    name = benches;
+    config = Criterion::default().significance_level(0.1).sample_size(10);
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
 
