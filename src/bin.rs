26e0cc1a73024a5706f63851f39be0ae4f4f2997
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2022-12-24 10:51:48 +0100

    Update from main

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,11 @@
 use std::env;
 
-use solutions::day23::{part1, part2};
+use solutions::day24::{part1, part2};
 
 fn main() {
     let session_key = env::var("AOC_SESSION").unwrap();
     let client = rudolf_rs::Client::new(session_key);
-    let input = client.get(2022, 23).unwrap();
+    let input = client.get(2022, 24).unwrap();
 
     println!("{}", part1(&input));
     println!("{}", part2(&input));
